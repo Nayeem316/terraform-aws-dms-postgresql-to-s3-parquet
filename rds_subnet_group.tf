7c897aab7fde313f7d33594a06aecaf9035d007b
Nayeem Syed <syed.nayeem316@gmail.com>
2025-12-26 18:20:10 -0500

    delete all files

--- rds_subnet_group.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_db_subnet_group" "rds_hybrid" {
-  name        = "rds-hybrid-subnet-group"
-  description = "Private subnets for RDS"
-  subnet_ids  = aws_subnet.private[*].id
-
-  tags = merge(local.common_tags, { Name = "rds-hybrid-subnet-group" })
-}
